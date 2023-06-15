048da32d268a25da9e40a3f6b2799dc2e4118e40
YamJr <yamgrg2058@gmail.com>
2023-06-15 16:15:13 +0545

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-import './App.css'
 
+import './App.css'
+import Footer from './components/Footer'
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-     
+     <h1>Hello</h1>
+     <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer(){
+    return(
+   <>
+   <div className="footer-container">
+        <div className="footer-main">
+              {/* <div className="footer-left">
+                    <p>Hello</p>
+              </div>
+              <div className="footer-center">
+                    <p>Happy</p>
+              </div>
+              <div className="footer-right">
+                    <p>There</p>
+              </div> */}
+        </div>
+        <div className="footer-bottom">
+            <p className='txt'>Copyright © 2023 Government of Nepal.All Rights Reserved. Singhdurbar, Kathmandu</p>
+        </div>
+   </div>
+   </>
+    )
+}
+export default Footer
